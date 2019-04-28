98d25590fe1dd6b3cba08a8e6f466b9f2b265f6d
Willie Johnson <liwa.johnson@gmail.com>
2019-04-28 17:28:12 -0400

    Add navmesh player mover script

---
+++ Assets/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+  [SerializeField] Transform target;
+
+  // Update is called once per frame
+  void Update()
+  {
+    NavMeshAgent agent = GetComponent<NavMeshAgent>();
+    agent.destination = target.position;
+  }
+}
